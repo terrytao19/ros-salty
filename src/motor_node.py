49f562a504d211e5a734ee7ab4dba9328c94b7c0
Terry <terrytao19@gmail.com>
2023-02-23 12:20:24 -0500

    motor node

---
+++ src/motor_node.py
@@ -0,0 +1,24 @@
+import rospy
+from std_msgs.msg import String, Header
+from vision_msgs.msg import Detection2DArray, Detection2D, BoundingBox2D
+from apriltag_ros.msg import *
+from sensor_msgs.msg import Image
+
+import cv2
+import math
+import random
+import sys
+import time
+
+class ReadWrite():
+    def __init__(self):
+        self.serial_topic = '/salty/move'
+        rospy.Subscriber(self.move_topic, Move, self.callback)
+        self.serial_pub = rospy.Publisher(self.serial_topic, String, queue_size=10)
+
+        rospy.loginfo("Motor Node Initialized")
+
+    def publish(self):
+        move_msg = String
+        move_msg.data = "r2000"
+        self.serial_pub.publish(move_msg)
